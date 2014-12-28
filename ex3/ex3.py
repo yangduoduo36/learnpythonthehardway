cc0bd49f36427308d8a1a0c45441b0c6d87387b3
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2014-12-28 16:18:25 +0530

    Adds ex3

---
+++ ex3/ex3.py
@@ -0,0 +1,36 @@
+## Python follows PEDMAS
+
+print "I will now count my chickens:"
+
+
+## First 30/ 6 = 5
+## Then 25+5 = 30
+print "Hens", 25 + 30 / 6 ## = 30
+
+## 25*3=75
+# 75%4 = 3
+# 100-3 = 97
+print "Roosters", 100 - 25 * 3 % 4 ## = 97
+
+print "Now I will count the eggs:"
+
+# 1/4 =0
+# 4%2 =0
+# 6-5+0-0+6 =7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 ## 7
+
+print "Is it true that 3 + 2 < 5 - 7?" 
+
+# 5<-2
+print 3 + 2 < 5 - 7 # False
+
+print "What is 3 + 2?", 3 + 2 #5
+print "What is 5 - 7?", 5 - 7 #-2
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 # True
+print "Is it greater or equal?", 5 >= -2 # True
+print "Is it less or equal?", 5 <= -2 # False
