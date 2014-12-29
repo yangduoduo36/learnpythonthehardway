5418defc59ba881df387266e9dcec7790a5a5637
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2014-12-29 14:23:51 +0530

    Adds ex12,13,14

---
+++ ex13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "Script called: ", script
+print "Arg1: ", first
+print "Arg2: ", second
+print "Arg3: ", third
---
+++ ex13/study_drills.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script_name, arg1 = argv
+
+print "Hi, ", arg1, raw_input("what is your age?")
