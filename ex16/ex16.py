c874cfcd1bf8cf1b2699503af6e84e8852158d91
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2014-12-29 17:55:14 +0530

    Adds ex16 partial

---
+++ ex16/ex16.py
@@ -0,0 +1,39 @@
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C"
+print "If you do want that, hit RETURN."
+raw_input("?")
+
+## Open file in write mode
+print "Opening the file..."
+target = open(filename, 'w')
+
+### Empty contents of file
+print "Truncating the file. Goodbye!"
+target.truncate()
+
+### 3 lines of content
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line1: ")
+line2 = raw_input("line2: ")
+line3 = raw_input("line3: ")
+
+print "I'm going to write these to the file."
+
+### Write 3 lines to the file
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+## Closing the file
+print "And finally we close it."
+target.close()
+
+
