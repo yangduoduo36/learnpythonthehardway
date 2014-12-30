46c1da6886b8ab5fbaf61c49f9ea18a19bfabc7b
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2014-12-30 20:12:29 +0530

    Adds ex27,28,29

---
+++ ex29/ex29.py
@@ -0,0 +1,29 @@
+people = 30
+cats = 20
+dogs = 15
+
+if people < cats:
+	print "Too many cats! the world is doomed!"
+
+if people > cats:
+	print "Not many cats! the world is saved!"
+
+if people < dogs:
+	print "The world is drooled on"
+
+if people > dogs:
+	print "The world is dry!"
+
+if people <= cats:
+	print "It's not catlandia!"
+
+dogs+=5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs"
+
+if people <= dogs:
+	print "People are less than or equal to dogs"
+
+if people == dogs:
+	print "People are dogs."
