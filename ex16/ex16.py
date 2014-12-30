9ebc80ae97f1b12132fb29b92d1efcb50c9e092a
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2014-12-30 09:24:55 +0530

    Adds ex16 remaining

--- ex16/ex16.py
+++ ex16/ex16.py
@@ -25,12 +25,16 @@ line3 = raw_input("line3: ")
 print "I'm going to write these to the file."
 
 ### Write 3 lines to the file
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+#target.write(line1)
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
+
+## Study drill - compress above target.write into 1
+
+target.write("%s\n%s\n%s\n" % (line1,line2,line3))
 
 ## Closing the file
 print "And finally we close it."
