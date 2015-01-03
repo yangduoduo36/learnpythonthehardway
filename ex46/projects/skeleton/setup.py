37bd88fb75a714f4fd5d18e53d1cd382f69309e4
Nitin Venkatesh <venkatesh.nitin@gmail.com>
2015-01-04 02:14:22 +0530

    Adds the skeleton

---
+++ ex46/projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setuptool
+except:
+	from distutils.core import setup
+
+config = {
+	'description': 'My Project',
+	'author': 'Nitin Venkatesh',
+	'url': 'http://nitstorm.github.io/',
+	'download_url': 'yet to be configured',
+	'author_email': 'xyz@gmail.com',
+	'version':'0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
